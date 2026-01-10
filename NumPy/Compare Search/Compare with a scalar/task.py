28b4119f7817f6d4860d0bad2573910d3cb1ced3
Ramon <rposilva.dev@gmail.com>
2026-01-10 11:33:09 -0300

    Mark task as solved and update placeholders and task implementation

--- NumPy/Compare Search/Compare with a scalar/task.py
+++ NumPy/Compare Search/Compare with a scalar/task.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 arr = np.arange(1000)
-filter_ = arr % 4 == 0 # TODO: create a boolean array
-filtered_arr = arr[filter_]# TODO: use it as a filter/mask
+filter_ = arr % 4 == 0
+filtered_arr = arr[filter_]
 
 
 if __name__ == '__main__':
