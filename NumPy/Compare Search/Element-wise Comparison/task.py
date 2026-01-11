a85384c46bf639feb72eab96f239723b3180a088
Ramon <rposilva.dev@gmail.com>
2026-01-11 00:14:43 -0300

    Mark task as solved and update placeholders and implementation Task: Comparação Elemento a Elemento

--- NumPy/Compare Search/Element-wise Comparison/task.py
+++ NumPy/Compare Search/Element-wise Comparison/task.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 a = np.arange(20).reshape(4, 5)
-b = # TODO
-compare_a_b = # TODO
+# [0,1,2,3,4]
+# [5,6,7,8,9]
+# [10,11,12,13,14]
+# [15,16,17,18,19]
+b = np.arange(0, 25, 6).reshape(5,)
+compare_a_b = np.equal(a, b)
 
 
 if __name__ == '__main__':
