e708d48696f91fefc2f67b3cebc86c35a619d0ad
Ramon <rposilva.dev@gmail.com>
2026-01-14 20:17:24 -0300

    Mark tasks as solved, update placeholders, and implement solution: Encontrar Máximos

--- NumPy/Compare Search/Find maximum/task.py
+++ NumPy/Compare Search/Find maximum/task.py
@@ -1,8 +1,12 @@
 import numpy as np
 
-data = # Import data
-maxima = # Find indices of maximum values in each row
-result = # Extract the maximum elements from each row
+data = np.loadtxt('data.csv', delimiter=',', dtype=np.int64) # Import data
+maxima = np.argmax(data, axis=1).reshape(-1, 1) # Find indices of maximum values in each row
+result = np.take_along_axis(data, maxima, axis=1) # Extract the maximum elements from each row
 
 if __name__ == '__main__':
-    print(result)
+    print('Dados \n', data)
+    print('\n')
+    print('Shape do data:', data.shape)
+    print('Sahpe do maxima:', maxima.shape)
+    print('\nResultado da Máxima\n', result)
