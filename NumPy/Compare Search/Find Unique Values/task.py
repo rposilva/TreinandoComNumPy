df21933ab1f96296fcaedce0cd70c07195f741d7
Ramon <rposilva.dev@gmail.com>
2026-01-16 18:23:46 -0300

    Mark tasks as solved, update placeholders, and implement solutions: Compare Search and Indexing Basics: Encontrar valores únicos.

--- NumPy/Compare Search/Find Unique Values/task.py
+++ NumPy/Compare Search/Find Unique Values/task.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-csv = # TODO
-data, labels = # TODO
+csv = np.genfromtxt('data.csv', delimiter=',', skip_header=1)
+data, labels = csv[:, 1:], csv[:, 0].astype(int)
 
-classes = # TODO
-unique_measurements, unique_data_counts = # TODO
+classes = np.unique(labels)
+unique_measurements, unique_data_counts = np.unique(data, return_counts=True)
 
-most_frequent_index = # TODO
-most_frequent_measurement = # TODO
+most_frequent_index = np.argmax(unique_data_counts)
+most_frequent_measurement = unique_measurements[most_frequent_index]
 
 if __name__ == "__main__":
-    print(classes)
-    print(unique_data_counts)
-    print(most_frequent_index)
-    print(most_frequent_measurement)
+    print(f"Classes encontradas:\n{classes}")
+    print(f"\nContagens de cada valor único:\n{unique_data_counts}")
+    print(f"\nÍndice do valor mais comum:\n{most_frequent_index}")
+    print(f"\nMedição mais frequente (Moda):\n{most_frequent_measurement}")
 
