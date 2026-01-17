972bc784b323ae35bf2e63e913ad646a59dfa808
Ramon <rposilva.dev@gmail.com>
2026-01-17 01:56:44 -0300

    Mark task as solved, update placeholders, and implement solution: Calculo de Entropia

--- NumPy/Array Math/Basic Math Functions/task.py
+++ NumPy/Array Math/Basic Math Functions/task.py
@@ -2,12 +2,12 @@ import numpy as np
 
 
 def calculate_entropy(data):
-    pass
+    data = np.array(data)
+    entropy = -np.sum(data * np.log2(data))
+    return entropy
 
 
 if __name__ == '__main__':
     probabilities = [0.16666667, 0.01754386, 0.05263158, 0.13157895, 0.16666667,
                      0.13157895, 0.14035088, 0.01754386, 0.12280702, 0.05263158]
     print(calculate_entropy(probabilities))
-
-
