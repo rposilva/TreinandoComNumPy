ab9fd4387258304274cb04c04e0b6edb1c0e629c
Ramon <rposilva.dev@gmail.com>
2026-01-25 13:47:42 -0300

    Mark tasks as solved, update placeholders, and implement solution: Separar Canais de Cor

--- Projects/SVD/task/task.py
+++ Projects/SVD/task/task.py
@@ -2,11 +2,24 @@ import matplotlib.pyplot as plt
 
 img = plt.imread('horse.jpg')  # Read image and transform it into a NumPy array.
 
-img_rescaled = img / 255.0
+img_rescaled = img / 255
 
-img_max, img_min = img.max(), img.min()
-rescaled_max, rescaled_min = img_rescaled.max(), img_rescaled.min()
+red_channel = img_rescaled[:, :, 0]
+green_channel = img_rescaled[:, :, 1]
+blue_channel = img_rescaled[:, :, 2]
 
 if __name__ == '__main__':
-    print(img_max, img_min)
-    print(rescaled_max, rescaled_min)
+    print('Green channel: ')
+    print(red_channel)
+    print('\nRed channel: ')
+    print(green_channel)
+    print('\nBlue channel: ')
+    print(blue_channel)
+    print(plt.imshow(img_rescaled[:, :, 0]))
+    print(plt.show())
+    print(plt.imshow(img_rescaled[:, :, 1]))
+    print(plt.show())
+    print(plt.imshow(img_rescaled[:, :, 2]))
+    print(plt.show())
+    print(plt.imshow(img_rescaled[:, :, :]))
+    print(plt.show())
