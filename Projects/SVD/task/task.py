61ae3e06354f89b7c75ff6fdd2d73b7b04df9d8a
Ramon <rposilva.dev@gmail.com>
2026-01-25 22:29:27 -0300

    Mark tasks as solved, update placeholders, and implement solution: Perform SVD on Grayscale Image

--- Projects/SVD/task/task.py
+++ Projects/SVD/task/task.py
@@ -1,4 +1,3 @@
-import numpy as np
 from numpy import linalg
 import matplotlib.pyplot as plt
 
@@ -6,9 +5,10 @@ img = plt.imread('horse.jpg')
 
 img_rescaled = img / 255
 
-img_gray = img_rescaled @ np.array([0.2126, 0.7152, 0.0722])
+img_gray = img_rescaled @ [0.2126, 0.7152, 0.0722]
+
+U, s, Vt = linalg.svd(img_gray)
 
 if __name__ == '__main__':
-    print(img_gray.shape)
-    plt.imshow(img_gray, cmap="gray")
-    plt.show()
+    print(U.shape, s.shape, Vt.shape)
+
