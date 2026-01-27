caabbde53f0cde59266953222db327be13ccd1e6
Ramon <rposilva.dev@gmail.com>
2026-01-27 17:43:37 -0300

    Mark tasks as solved, update placeholders, and implement solution: Implement low-rank approximation using SVD. Último d do curso de Numpy.

--- Projects/SVD/task/task.py
+++ Projects/SVD/task/task.py
@@ -9,21 +9,19 @@ img_rescaled = img / 255
 img_array_transposed = np.transpose(img_rescaled, (2, 0, 1))
 U, s, Vt = linalg.svd(img_array_transposed)
 
-Sigma = np.zeros_like(img_array_transposed)
-for i in range(3):
-    np.fill_diagonal(Sigma[i], s[i])
+Sigma = np.zeros((3, 408, 612))
+for j in range(3):
+    np.fill_diagonal(Sigma[j, :, :], s[j, :])
 
 reconstructed = U @ Sigma @ Vt
 
+reconstructed = np.clip(reconstructed, 0, 1)
+
+k = 10
+approx_img = U[..., :k] @ Sigma[..., :k, :k] @ Vt[..., :k, :]
+
+plt.imshow(np.transpose(approx_img, (1, 2, 0)))
 
 if __name__ == '__main__':
-    print('Shape of the reconstructed image array: ', reconstructed.shape)
-    print('Minimum value in the reconstructed image array: ', reconstructed.min())
-    print('Maximum value in the reconstructed image array: ', reconstructed.max())
-    # This assertion will fail if your reconstruction isn't close to the original:
-    np.testing.assert_array_almost_equal(reconstructed, img_array_transposed)
-
-    reconstructed = np.clip(reconstructed, 0, 1)
-    plt.imshow(np.transpose(reconstructed, (1, 2, 0)))
+    print(approx_img.shape)
     plt.show()
-
